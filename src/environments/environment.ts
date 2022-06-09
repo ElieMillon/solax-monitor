f5dd562f0e784568d98f17c7268c31fc42944566
Elie Millon <elie.millon@gmail.com>
2022-06-09 22:15:57 +0200

    Add weather component

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  place: 'Paris, Île-de-France, France',
+  degreeType: 'C',
+  weatherRefreshInterval: 20 * 60
 };
 
 /*
